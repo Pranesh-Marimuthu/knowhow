4bd7ca12746908fd5844108f57c3b8934baebab8
pranesh-marimuthu <praneshmu@gmail.com>
2025-02-27 10:31:05 +0530

    feat(header) - Add header component with unit tests

---
+++ src/app/core/layout/header/header.component.spec.ts
@@ -0,0 +1,28 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HeaderComponent } from './header.component';
+import { By } from '@angular/platform-browser';
+
+describe('HeaderComponent', () => {
+  let component: HeaderComponent;
+  let fixture: ComponentFixture<HeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [HeaderComponent]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(HeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should display welcome message', () => {
+    const headerElement = fixture.debugElement.query(By.css('header'));
+    expect(headerElement.nativeElement.textContent.trim()).toBe('Welcome to know how site :)');
+  });
+});
